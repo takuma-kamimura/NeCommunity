aa9a55b4381a4451910a88393a4774ea3c878057
上村拓磨 <kamimuratakuma@MacBook-Pro.local>
2023-11-02 14:45:40 +0900

    Rails-New-TopPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'tops/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "tops#top"
 end
