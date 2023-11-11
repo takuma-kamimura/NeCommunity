1a0738327b26d6c621214cbba0c72f68cef6dcbe
上村拓磨 <kamimuratakuma@MacBook-Pro.local>
2023-11-11 15:50:29 +0900

    追加修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   belongs_to :cat, optional: true  # Optional: trueを使用して、Catが必須でないことを指定
   mount_uploader :photo, PostPhotoUploader # アバターアップローダー、useで使ってるものと同じものを使う。
 
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 500 }
+
   def self.ransackable_attributes(auth_object = nil)
     %w[title body]  # 検索可能な属性を指定してください
   end
