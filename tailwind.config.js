d763ac24f60cc70838c29421beb019cb2d465f85
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2023-11-22 16:05:09 +0900

    トグルリスト機能&daisyuiのインストールまで

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  
+  plugins: [require("daisyui")],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+  ],
+  variants: {
+    visibility: ['responsive', 'hover', 'focus', 'group-hover']
+  }
 }
