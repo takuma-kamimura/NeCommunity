b50d149107888c5920f3cfadd051eca8297c1de4
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2023-11-28 12:11:37 +0900

    ページタイトル追加・動的に変更処理追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
 module ApplicationHelper
-    def flash_class(key)
-        case key
-        when 'success'
-          'bg-green-100 border-green-400 text-green-700'
-        when 'danger', 'error'
-          'bg-red-100 border-red-400 text-red-700'
-        else
-          'bg-blue-100 border-blue-400 text-blue-700'
-        end
-      end
+  def page_title(title = '')
+    base_title = 'NeCommunity'
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
+
+  def flash_class(key)
+    case key
+    when 'success'
+      'bg-green-100 border-green-400 text-green-700'
+    when 'danger', 'error'
+      'bg-red-100 border-red-400 text-red-700'
+    else
+      'bg-blue-100 border-blue-400 text-blue-700'
+    end
+  end
 end
