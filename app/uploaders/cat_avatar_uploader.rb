9a68199056fc49b667de3a804d3cd86678dc30e5
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2023-11-28 19:54:05 +0900

    デフォルト画像修正

--- app/uploaders/cat_avatar_uploader.rb
+++ app/uploaders/cat_avatar_uploader.rb
@@ -1,4 +1,4 @@
-class AvatarUploader < CarrierWave::Uploader::Base
+class CatAvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -9,20 +9,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-
-  # def store_dir
-  # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
-
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
+
   def default_url # デフォルトの画像ファイル
-    'kkrn_icon_user_1.png'
+    'ねこフリー画像.jpeg'
   end
-  
+
   def extension_whitelist # 拡張子の制限
     %w[jpg jpeg gif png]
   end
@@ -32,7 +26,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   else
     storage :file             # 開発・テスト時にはローカルにファイルを保存する
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
