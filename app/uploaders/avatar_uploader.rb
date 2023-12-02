18ba17e9317e704183e6f2365a88072cd50a2bfd
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2023-12-02 16:37:23 +0900

    アップローダー処理修正完了

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -23,26 +23,28 @@ class AvatarUploader < CarrierWave::Uploader::Base
     'kkrn_icon_user_1.png'
   end
   
-  version :index_size do
-    process resize_and_pad: [1600, 900, '#f5ebdc', 'Center']
-    process :convert_to_webp
-  end
+  # version :index_size do
+  #   process resize_and_pad: [1600, 900, '#f5ebdc', 'Center']
+  #   process :convert_to_webp
+  # end
 
-  def convert_to_webp
-    manipulate! { |img| img.format('webp') }
-  end
+  # def convert_to_webp
+  #   manipulate! { |img| img.format('webp') }
+  # end
 
   def filename
     return unless original_filename.present?
 
+    # base_name = File.basename(original_filename, '.*')
     base_name = File.basename(original_filename, '.*')
+    "#{base_name}.webp"
   end
 
   def extension_whitelist # 拡張子の制限
     %w[jpg jpeg gif png heic webp]
   end
 
-  if Rails.env.production?    
+  if Rails.env.production?
     storage :fog              # 本番時にS3にファイルを保存する
   else
     storage :file             # 開発・テスト時にはローカルにファイルを保存する
