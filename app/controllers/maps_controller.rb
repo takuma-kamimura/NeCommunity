5a4bdcc493200dd4429546be33e7f420dc8742a6
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2023-12-26 13:53:41 +0900

    Google Map 検索処理途中まで

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,6 +3,7 @@ class MapsController < ApplicationController
   def index
     # Google Maps Places API を使用して検索
     @search_results = GoogleMapsService.search_shops_by_location(params[:location])
+    @search_results_json = @search_results.to_json
   end
 
 end
--- app/services/google_maps_service.rb
+++ app/services/google_maps_service.rb
@@ -4,7 +4,7 @@ class GoogleMapsService
     require "uri"
   
     BASE_URL = 'https://maps.googleapis.com/maps/api/place'
-    API_KEY = ENV['YOUR_API_KEY']
+    API_KEY = ENV['PLACES_API_KEY']
     LANGUAGE = 'ja'.freeze
   
     def self.search_shops_by_location(location)
@@ -12,16 +12,22 @@ class GoogleMapsService
       uri = URI.parse("#{BASE_URL}/textsearch/json?query=#{query}&key=#{API_KEY}&language=#{LANGUAGE}")
       res = Net::HTTP.get_response(uri)
       parsed_body = JSON.parse(res.body)
-      formatted_results = format_results(JSON.parse(res.body))
+      #formatted_results = format_results(JSON.parse(res.body)) コメントにしてページに表示されなくなった
       # "<ul>#{formatted_results}</ul>"
+    #   binding.pry
     end
   
     private
   
     def self.format_results(results)
-      # 結果をHTMLに整形するロジックを追加
-      results['results'].map do |result|
-        "<li>#{result['name']}: #{result['formatted_address']}</li>"
-      end.join('')
-    end
+        # 結果をHTMLに整形するロジックを追加
+        results['results'].map do |result|
+          name = result['name']
+          address = result['formatted_address']
+          latitude = result['geometry']['location']['lat']
+          longitude = result['geometry']['location']['lng']
+      
+          "<li>#{name}: #{address} (緯度: #{latitude}, 経度: #{longitude})</li>"
+        end.join('')
+      end
   end
