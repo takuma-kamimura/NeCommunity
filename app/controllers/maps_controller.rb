dbc3e5f10b5c3fe26654544a2eaf041263d6e907
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2023-12-26 15:38:35 +0900

    表示不完全_途中

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,6 +4,9 @@ class MapsController < ApplicationController
     # Google Maps Places API を使用して検索
     @search_results = GoogleMapsService.search_shops_by_location(params[:location])
     @search_results_json = @search_results.to_json
+    # binding.pry
+    render json: { search_results: @search_results }
+    # render :index
   end
 
 end
