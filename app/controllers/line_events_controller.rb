d48be521d5e61d90b563b98ba77e7317ca116dce
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-01-10 17:25:53 +0900

    Lineログイン機能の実装4(途中まで)

--- app/controllers/line_events_controller.rb
+++ app/controllers/line_events_controller.rb
@@ -16,10 +16,23 @@ class LineEventsController < ApplicationController
     def receive
       body = request.body.read
       events = client.parse_events_from(body)
-      events.each { |event|
-        userId = event['source']['userId']  #userId取得
-        p 'UserID: ' + userId # UserIdを確認
-      }
+    #   events.each { |event|
+    #     userId = event['source']['userId']  #userId取得
+    #     p 'UserID: ' + userId # UserIdを確認
+    #   }
+      events.each do |event|
+        if event['type'] == 'follow'
+            # p '成功'
+        end
+      end
+    #   events.each { |event|
+    #   userId = event['source']['userId']  #userId取得
+    #   p 'UserID: ' + userId # UserIdを確認
+    #   if event == Line::Bot::Event::Follow
+    #     userId = event['source']['userId']
+    #     p 'UserIDの確認: ' + userId # UserIdを確認
+    #   end
+    # }
     # events.each do |event|
     #     if event.is_a?(Line::Bot::Event::Message) && event.type == Line::Bot::Event::MessageType::Text
     #       user_id = event['source']['userId']
