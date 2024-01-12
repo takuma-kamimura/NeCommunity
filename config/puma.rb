e1057a6c318e2d1a8025e888c2f5dbf0aeb4d404
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-01-12 10:06:35 +0900

    修正8

--- config/puma.rb
+++ config/puma.rb
@@ -16,9 +16,10 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 if ENV['RAILS_ENV'] == 'production'
+  # 本番環境
   port ENV.fetch("PORT") { 3000 }
-  # bind "ssl://0.0.0.0:3000?key=config/certs/localhost-key.pem&cert=config/certs/localhost.pem"
 else
+  # 開発環境
   bind "ssl://0.0.0.0:" + ENV["PORT"].to_s + "?key=config/certs/localhost-key.pem&cert=config/certs/localhost.pem"
   bind "tcp://0.0.0.0:3000"
 end
