ba21853799f2d0e4f1a0ec7cb0f334e5cd0abbcb
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-02-08 21:14:42 +0900

    デプロイ失敗により修正

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import { enter, leave } from "./transition"
+// import { enter, leave } from "./transition"
 
 export default class extends Controller {
   static targets = ['container', 'background']
