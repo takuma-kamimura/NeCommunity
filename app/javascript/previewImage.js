3f11c197b0a0653f958a4ff1dae4e3cf71246b98
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-02-14 16:46:48 +0900

    画像プレビュー機能のHEIC対応をマイ猫sにも適応

--- app/javascript/previewImage.js
+++ app/javascript/previewImage.js
@@ -1,20 +1,4 @@
-import heic2any from 'heic2any';
-
-// previewImage.js
-// document.addEventListener("DOMContentLoaded", function () {
-//   // ファイルフィールドから選択された画像をプレビューする関数
-//   window.previewImage = function(input) {
-//     const preview = document.getElementById("preview");
-//     if (input.files && input.files[0]) {
-//       const reader = new FileReader();
-//       reader.onload = function (e) {
-//         preview.src = e.target.result;
-//       };
-//       reader.readAsDataURL(input.files[0]);
-//     }
-//   }
-// });
-
+import heic2any from "heic2any";
 
 document.addEventListener("DOMContentLoaded", function () {
   // ファイルフィールドから選択された画像をプレビューする関数
@@ -25,59 +9,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
       reader.onload = async function (e) {
         if (input.files[0].type === "image/heic") {
+          // HEIC画像を表示する前にjpegへ変換する
           const heicBlob = input.files[0]; // HEIC形式のBlobデータ
-          const jpegBlob = await heic2any({ blob: heicBlob, toType: "image/webp" });
+          // awaitキーワードを使用し、heic2any関数の実行結果をjpegBlobに取得
+          const jpegBlob = await heic2any({ blob: heicBlob, toType: "image/jpeg" });
           preview.src = URL.createObjectURL(jpegBlob);
-          // // HEIC画像を表示する前に変換する
-          // convertHeicToWebp(e.target.result, function (convertedDataUrl) {
-          //   preview.src = convertedDataUrl;
-          // });
         } else {
           // HEIC以外の画像はそのまま表示
           preview.src = e.target.result;
         }
       };
-
       reader.readAsDataURL(input.files[0]);
     }
   };
-
-  // HEIC画像をWebPに変換する関数
-  function convertHeicToWebp(heicDataUrl, callback) {
-    const img = new Image();
-    img.onload = function () {
-      console.log("Image loaded");
-      // Canvasを使ってWebPに変換
-      const canvas = document.createElement("canvas");
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-      const webpDataUrl = canvas.toDataURL("image/webp");
-      callback(webpDataUrl);
-    };
-    img.onerror = function (e) {
-      console.error("Image failed to load:", e);
-      console.error("Image source:", img.src);
-    };
-    img.src = heicDataUrl;
-  }
 });
 
 // モーダルウィンドウ実装後
 document.addEventListener("DOMContentLoaded", function () {
   // ファイルフィールドから選択された画像をプレビューする関数
-  window.previewImage2 = function (input, catId) {
+  window.previewImage2 = async function (input, catId) {
     const preview = document.getElementById("preview-" + catId);
     if (input.files && input.files[0]) {
       const reader = new FileReader();
 
-      reader.onload = function (e) {
+      reader.onload = async function (e) {
         if (input.files[0].type === "image/heic") {
-          // HEIC画像を表示する前に変換する
-          convertHeicToWebp(e.target.result, function (convertedDataUrl) {
-            preview.src = convertedDataUrl;
-          });
+         // HEIC画像を表示する前にjpegへ変換する
+         const heicBlob = input.files[0]; // HEIC形式のBlobデータ
+         // awaitキーワードを使用し、heic2any関数の実行結果をjpegBlobに取得
+         const jpegBlob = await heic2any({ blob: heicBlob, toType: "image/jpeg" });
+         preview.src = URL.createObjectURL(jpegBlob);
         } else {
           // HEIC以外の画像はそのまま表示
           preview.src = e.target.result;
@@ -87,25 +48,4 @@ document.addEventListener("DOMContentLoaded", function () {
       reader.readAsDataURL(input.files[0]);
     }
   };
-
-  // HEIC画像をWebPに変換する関数
-  function convertHeicToWebp(heicDataUrl, callback) {
-    const img = new Image();
-    img.onload = function () {
-      console.log("Image loaded");
-      // Canvasを使ってWebPに変換
-      const canvas = document.createElement("canvas");
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-      const webpDataUrl = canvas.toDataURL("image/webp");
-      callback(webpDataUrl);
-    };
-    img.onerror = function (e) {
-      console.error("Image failed to load:", e);
-      console.error("Image source:", img.src);
-    };
-    img.src = heicDataUrl;
-  }
 });
