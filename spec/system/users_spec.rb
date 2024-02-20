b585efd306cf8e346f32fe628e82f6a14f9b1e04
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-02-20 15:08:25 +0900

    scssとtailwind cssが干渉しないように設定を追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   before do
-    # visit new_user_path
+    user = build(:user)
+    visit new_user_path
     # fill_in 'user[email]', with: user.email
     # fill_in 'user[password]', with: user.password
     # click_button 'ログイン'
@@ -10,8 +11,10 @@ RSpec.describe "Users", type: :system do
   end
   describe 'ユーザー新規登録時の表示テスト' do
     it "名前がない場合はエラーメッセージが表示されること" do
+      fill_in 'user[name]', with: nil
       # fill_in 'user[email]', with: user.email
       # fill_in 'user[password]', with: user.password
+      # fill_in 'user[password_confirmation]', with: user.password_confirmation
       # visit user_posts_path(user, post)
     end
   end
