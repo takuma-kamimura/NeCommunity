559701d8fef7389423911072d6e032e90554aa05
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-03-03 22:50:03 +0900

    rspecのグーグルマップの詳細表示テストまで完成

--- spec/system/google_maps_spec.rb
+++ spec/system/google_maps_spec.rb
@@ -3,23 +3,34 @@ require 'rails_helper'
 RSpec.describe "GoogleMaps", type: :system do
   let(:user) { create(:user) }
   before do
+    sleep 5
     login_process(user)
     find('#bars').click
     visit search_maps_path
   end
   describe '動物病院の検索テスト', js: true do
-    context 'ログアウト状態の場合' do
+    context 'マップの検索テスト' do
       it "好きな地名を押すと、mapにマーカーが表示され、病院がリストとなって表示されること" do
+        expect(current_path).to eq(search_maps_path)
         fill_in 'location', with: '札幌'
         click_button '検索'
-        # sleep 5
         expect(page).to have_selector('#hospital')
         expect(page).to have_css('map#gmimap0')
+      end
+    end
+    context 'グーグルマップで検索後の詳細ページ表示テスト' do
+      it "好きな地名を押すと、mapにマーカーが表示され、病院がリストとなって表示され、リンクを押すと選択した動物病院の詳細ページへ遷移できること" do
         expect(current_path).to eq(search_maps_path)
-        # map#gmimap0 area", visible: false
-        # # expect(page).to have_css('div[title="まつい犬猫病院"]', visible: true)
-        # expect(page).to have_css('div[title="まつい犬猫病院"]', visible: true, wait: 30)
-        puts page.body
+        fill_in 'location', with: '札幌緑が丘動物病院'
+        click_button '検索'
+        expect(page).to have_selector('#hospital')
+        expect(page).to have_css('map#gmimap0')
+
+        click_link('札幌緑が丘動物病院の詳細ページ')
+        expect(page).to have_content('札幌緑が丘動物病院')
+        expect(page).to have_content('北海道札幌市南区澄川５条１１丁目２−２')
+        expect(current_path).to eq('/maps/search')
+        expect(page).to have_content('公式HPはこちら')
       end
     end
   end
