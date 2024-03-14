e1e9ad6a9f02ff0c59ecd4d56fed8be7350112ba
上村拓磨 <jjd.swt-c.5@docomo.ne.jp>
2024-03-14 20:27:46 +0900

    投稿のrspec追加2

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -218,12 +218,11 @@ RSpec.describe 'Posts', type: :system do
       expect(page).to have_css("#button-edit-#{me_post.id}")
       expect(current_path).to eq(post_path(me_post))
     end
-    it '他人が投稿した投稿の詳細ページが表示され、投稿者のモーダルウィンドウが表示され投稿者のマイ猫sのリンクがあり、リンク先で投稿者の猫一覧が確認できること。' do
+    it '他人が投稿した投稿の詳細ページで、投稿者の名前をクリックするとモーダルウィンドウが表示され、投稿者のマイ猫sのリンクがあり、リンク先で投稿者の猫一覧が確認できること。' do
       visit post_path(post)
       expect(page).to have_content(post.title)
       expect(page).to have_content(post.body)
       expect(page).to have_link(another_user.name)
-
 
       click_button another_user.name
       expect(page).to have_content('飼い主さんのお名前')
@@ -233,6 +232,21 @@ RSpec.describe 'Posts', type: :system do
       expect(current_path).to eq(usercat_users_path)
       expect(page).to have_content("#{another_user.name}さんのマイ猫\'s")
       expect(page).to have_content(cat.name)
+    end
+    it '自分が投稿した投稿の詳細ページが表示され、投稿者の名前(自分の名前)をクリックするとモーダルウィンドウが表示され、自分のマイ猫sのリンクがあり、リンク先で自分の猫一覧が確認できること。' do
+      visit post_path(me_post)
+      expect(page).to have_content(me_post.title)
+      expect(page).to have_content(me_post.body)
+      expect(page).to have_link(user.name)
+
+      click_button user.name
+      expect(page).to have_content('飼い主さんのお名前')
+      expect(current_path).to eq(post_path(me_post))
+      click_link "あなたのマイ猫\'s"
+      sleep 2
+      expect(current_path).to eq(cats_path)
+      expect(page).to have_content("マイ猫\'s")
+      expect(page).to have_content(me_cat.name)
     end
     it '投稿の詳細ページで猫のモーダルウィンドウが表示され、猫の名前のリンクを押すとその猫の投稿一覧が表示される' do
       visit post_path(post)
